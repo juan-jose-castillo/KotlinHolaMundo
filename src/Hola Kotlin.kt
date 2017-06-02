1fb627ab6b127776e8c7732702f773b282fc1594
castillocode <jjandroid2015@gmail.com>
2017-06-02 11:35:56 -0400

    Se agrego comentarios sobre el codigo de Hola mundo

--- src/Hola Kotlin.kt
+++ src/Hola Kotlin.kt
@@ -1,9 +1,12 @@
 /**
  * Created by juanjosecastillo on 2/6/17.
  */
+// palabra fun define la funcion
+
+///////orden de funcion es (NOMBRE DE PARAMETRO : TIPO PARAMENTRO)
 
 fun main(args:Array<String>)
 {
 
-    println("Hola mundo con kotlin")
+    println("Hola mundo con kotlin")// NO HAY PUNTO Y COMA
 }
